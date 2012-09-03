d1cc6d34d09393178ae28af7ce3524d62a73c7a1
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2012-09-02 21:02:12 -0500

    Throw in the finishing touches for now

--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,74 @@
-var docco = require( "docco" );
+var docco = require("docco");
 
-docco.run();
+// ###anvil.docco
+// author: Jonathan Creamer
+var doccoFactory = function( _, anvil ) {
+    return anvil.plugin({
+        name: "anvil.docco",
+        activity: "post-process",
+        all: true,
+        inclusive: false,
+        exclusive: false,
+        fileList: [],
+        commander: [
+            ["-d, --docco", "document all files"]
+        ],
+        // Configure the plugin
+        configure: function(config, command, done) {
+            var anvilConfig = config[ "anvil.config" ],
+                doccoConfig = config[ "anvil.docco" ];
 
-//console.log( process.argv.splice(2) );
+            if ( anvilConfig ) {
+                if ( doccoConfig.all) {
+                    this.all = true;
+                } else if ( doccoConfig.include) {
+                    this.inclusive = true;
+                    this.fileList = doccoConfig.include;
+                } else if ( doccoConfig.exclude ) {
+                    this.exclusive = true;
+                    this.fileList = doccoConfig.exclude;
+                }
+            } else if ( command.docco ) {
+                this.all = true;
+            }
+            done();
+        },
+        // Run the plugin
+        run: function( done ) {
+            var jsFiles = [];
+            if ( this.inclusive ) {
+                jsFiles = this.inclusive;
+            } else if ( this.all || this.exclusive ) {
+                jsFiles = _.filter( anvil.project.files, function(file) {
+                    return file.extension() === ".js";
+                });
+
+                if ( this.exclusive ) {
+                    jsFiles = _.reject( jsFiles, function(file) {
+                        return _.any( this.fileList, function(excluded) {
+                            return excluded.fullPath === file.fullPath;
+                        });
+                    });
+                }
+            }
+            if ( jsFiles.length > 0 ) {
+                anvil.log.step( "Documenting " + jsFiles.length + " files" );
+                anvil.scheduler.parallel( jsFiles, this.doc, function() {
+                    done();
+                });
+            } else {
+                done();
+            }
+        },
+
+        doc: function( file, done ) {
+            var self = this;
+            
+            docco.document([ file.fullPath ], {
+                output: "./lib/"
+            });
+        }
+    });
+};
+
+module.exports = doccoFactory;
