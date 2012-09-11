686b684689e8a321a42608777632b2cb5f8f69f7
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2012-09-11 15:15:59 -0500

    Add in new configuration options, document self

--- src/plugin.js
+++ src/plugin.js
@@ -6,19 +6,20 @@ var doccoFactory = function( _, anvil ) {
     return anvil.plugin({
         name: "anvil.docco",
         activity: "post-process",
-        all: true,
+        all: false,
         inclusive: false,
         exclusive: false,
+        output: "./docs/",
         fileList: [],
         commander: [
             ["-d, --docco", "document all files"]
         ],
         // Configure the plugin
         configure: function(config, command, done) {
-            var anvilConfig = config[ "anvil.config" ],
-                doccoConfig = config[ "anvil.docco" ];
+            var doccoConfig = config[ "docco" ];
+            
+            if ( doccoConfig ) {
 
-            if ( anvilConfig ) {
                 if ( doccoConfig.all) {
                     this.all = true;
                 } else if ( doccoConfig.include) {
@@ -28,22 +29,34 @@ var doccoFactory = function( _, anvil ) {
                     this.exclusive = true;
                     this.fileList = doccoConfig.exclude;
                 }
-            } else if ( command.docco ) {
+
+                if ( doccoConfig.output ) {
+                    this.output = doccoConfig.output;
+                }
+            }
+            else if ( command.docco ) {
                 this.all = true;
             }
+
+
             done();
         },
         // Run the plugin
         run: function( done ) {
             var jsFiles = [];
+            
+            // Include these files
             if ( this.inclusive ) {
                 jsFiles = this.inclusive;
-            } else if ( this.all || this.exclusive ) {
+            }
+            // Get all the files, then exclude the one's we don't want.
+            else if ( this.all || this.exclusive ) {
                 jsFiles = _.filter( anvil.project.files, function(file) {
                     return file.extension() === ".js";
                 });
 
                 if ( this.exclusive ) {
+                    // REJECTION!
                     jsFiles = _.reject( jsFiles, function(file) {
                         return _.any( this.fileList, function(excluded) {
                             return excluded.fullPath === file.fullPath;
@@ -51,21 +64,40 @@ var doccoFactory = function( _, anvil ) {
                     });
                 }
             }
+            
+            // Schedule the documentation.
             if ( jsFiles.length > 0 ) {
                 anvil.log.step( "Documenting " + jsFiles.length + " files" );
-                anvil.scheduler.parallel( jsFiles, this.doc, function() {
+                
+                this.doc( jsFiles, function() {
                     done();
                 });
+                
+                // anvil.scheduler.parallel( jsFiles, this.doc, function() {
+                //     done();
+                // });
             } else {
                 done();
             }
         },
-
-        doc: function( file, done ) {
+        // ### doc
+        // This is where the magic happens.
+        doc: function( files, done ) {
             var self = this;
             
-            docco.document([ file.fullPath ], {
-                output: "./lib/"
+            // Gotta make sure the output is correct.
+            anvil.fs.ensurePath( self.output, function( err ) {
+                if ( err ) {
+                    anvil.log.error( "Could not create a docs directory" );
+                    return;
+                }
+                docco.document( _.map(files, function(file) {
+                    return file.fullPath;
+                }), {
+                    output: self.output
+                });
+
+                done();
             });
         }
     });
